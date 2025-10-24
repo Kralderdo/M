2f7010b1c23e6146750fb7404d7b64ca0c2cc3f0
Kralderdo <kralderdo23@gmail.com>
2025-10-24 13:00:54 +0300

    Update __init__.py

--- ArchMusic/plugins/__init__.py
+++ ArchMusic/plugins/__init__.py
@@ -1,31 +1,14 @@
-#
-# Copyright (C) 2021-2023 by ArchBots@Github, < https://github.com/ArchBots >.
-#
-# This file is part of < https://github.com/ArchBots/ArchMusic > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/ArchBots/ArchMusic/blob/master/LICENSE >
-#
-# All rights reserved.
-#
+# ArchMusic/plugins/__init__.py
+# Düzeltilmiş Plugin Yükleyici – Alt klasör hatasını düzeltir
 
 import glob
-from os.path import dirname, isfile
+import os
 
+ALL_MODULES = []
 
-def __list_all_modules():
-    work_dir = dirname(__file__)
-    mod_paths = glob.glob(work_dir + "/*/*.py")
+for f in glob.glob("ArchMusic/plugins/*.py"):
+    module_name = os.path.basename(f)[:-3]
+    if module_name != "__init__":
+        ALL_MODULES.append(module_name)
 
-    all_modules = [
-        (((f.replace(work_dir, "")).replace("/", "."))[:-3])
-        for f in mod_paths
-        if isfile(f)
-        and f.endswith(".py")
-        and not f.endswith("__init__.py")
-    ]
-
-    return all_modules
-
-
-ALL_MODULES = sorted(__list_all_modules())
-__all__ = ALL_MODULES + ["ALL_MODULES"]
+__all__ = ALL_MODULES
