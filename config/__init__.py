fcb180e0af727f0fb1c7f8c89ceb760c5b91ad4d
Kralderdo <kralderdo23@gmail.com>
2025-10-24 18:15:12 +0300

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,6 +1,24 @@
-from .config import *
+import os
+from dotenv import load_dotenv
 
-# ... diğer import/ayarlar ...
+load_dotenv()
 
-# Log Grubu
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002970992169"))  # <- sabit varsayılan
+# Temel Bot Ayarları
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Sahipler
+OWNER_ID = int(os.getenv("OWNER_ID", "6366762649"))
+SUDO_USERS = list(map(int, os.getenv("SUDO_USERS", "6366762649").split()))
+
+# Asistan STRING
+STRING1 = os.getenv("STRING1")
+
+# Log ve Destek
+LOG_GROUP_ID = int(os.getenv("LOG_GROUP_ID", "-1002970992169"))
+SUPPORT_CHANNEL = os.getenv("SUPPORT_CHANNEL", "https://t.me/Los_Angaras_06")
+SUPPORT_GROUP = os.getenv("SUPPORT_GROUP", "https://t.me/Pars_Sohbet_TR")
+
+# Bot adı
+MUSIC_BOT_NAME = os.getenv("MUSIC_BOT_NAME", "Pars Müzik Bot")
